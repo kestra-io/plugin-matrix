fb1ba1c2ab63f0ec1f6dbfd4e93c5314a39b7a13
François Delbrayelle <fdelbrayelle@kestra.io>
2026-07-03 13:26:00 +0200

    chore: initial scaffold cleanup for plugin-matrix

---
+++ src/main/java/io/kestra/plugin/matrix/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Matrix",
+    description = "Matrix plugin for Kestra",
+    categories = PluginSubGroup.PluginCategory.DATA
+)
+package io.kestra.plugin.matrix;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
